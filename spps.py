a9834027c4151a7347a878d7ba6041ace6af497b
truesonofagun <>
2020-09-15 22:04:39 -0600

    editted the spps.py file add udp connection option in command and made tcp default changed execption to work properly and be more specific change py docs for func scanner"

--- spps.py
+++ spps.py
@@ -6,23 +6,39 @@ import argparse
 import sys
 
 
-def scanner(tcpHost, tcpPort):
-    """This uses a TCP connection to see if port is open
-    will info passed through tcpHost and tcpPort var"""
-    try:
-        _s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        _s.connect((tcpHost, int(tcpPort)))
-        print('[+]%s/tcp open' % tcpHost)
-        _s.close()
+def scanner(_addr, _port, _udp):
+    """This uses a TCP (or UDP if specified) connection to see if port is open
+    will info passed through addr and port var"""
+    if _udp is True:
+        try:
+            _s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            _s.connect((_addr, int(_port)))
+            print('[+]%s/udp open' % _addr)
+            _s.close()
 
-    except:
-        print('[-]%s/tcp closed' % tcpPort)
+        except ConnectionRefusedError:
+            print('[-]%s/udp closed' % _port)
+
+        except Exception as _e:
+            print(_e)
+    else:
+        try:
+            _s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            _s.connect((_addr, int(_port)))
+            print('[+]%s/tcp open' % _addr)
+            _s.close()
+
+        except ConnectionRefusedError:
+            print('[-]%s/tcp closed' % _port)
+
+        except Exception as _e:
+            print(_e)
+
 
 
 def parserVar():
     parser = argparse.ArgumentParser(description='Simple Python Port Scanner')
-    #parser.add_argument('-t', '--tcp', help='preform a TCP port scan', action='store_true', default=False)
-    #parser.add_argument('-u', '--udp', help='preform a UDP port scan', action='store_true', default=False)
+    parser.add_argument('-u', '--udp', help='preform a UDP port scan insteed of default tcp scan', action='store_true', default=False)
     parser.add_argument('-p', '--port', help='given port for scanning', type=int, default=None)
     parser.add_argument('address', help='IPv4 address in format x.x.x.x:xx')
 
@@ -37,14 +53,12 @@ def parserVar():
             print('Need to specify a port within the address as x.x.x.x:xx\n' +
                     'or with the -p/--port argument')
             sys.exit()
-        except e as Exception:
-            print(e)
+        except Exception as _e:
+            print(_e)
 
     return parsed
 
 
 if __name__ == '__main__':
-    _c = parserVar()
-    _a = _c.address
-    _p = _c.port
-    scanner(_a, _p)
+    _input = parserVar()
+    scanner(_input.address, _input.port, _input.udp)
